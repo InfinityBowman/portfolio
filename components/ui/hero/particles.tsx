4bb8bb71920b5ea02a0869e5556e752ab42ab400
Jacob Maynard <jacobamaynard@proton.me>
2025-01-05 16:28:09 -0700

    Test getcomputedstyle

--- components/ui/hero/particles.tsx
+++ components/ui/hero/particles.tsx
@@ -1,11 +1,9 @@
 "use client";
 import { useEffect, useMemo, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { type Container, type ISourceOptions, MoveDirection, OutMode } from "@tsparticles/engine";
-// import { loadAll } from "@tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+import { type ISourceOptions, MoveDirection, OutMode } from "@tsparticles/engine";
+
+import { loadSlim } from "@tsparticles/slim";
 
 export const ParticlesBackground = () => {
   const [init, setInit] = useState(false);
@@ -13,25 +11,24 @@ export const ParticlesBackground = () => {
   // this should be run only once per application lifetime
   useEffect(() => {
     initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
-      //await loadFull(engine);
       await loadSlim(engine);
-      //await loadBasic(engine);
     }).then(() => {
       setInit(true);
     });
   }, []);
 
-  // const bgColor = getComputedStyle(document.documentElement).getPropertyValue("--background-color").trim();
+  const bgColor = useMemo(() => {
+    if (typeof window !== "undefined") {
+      return getComputedStyle(document.documentElement).getPropertyValue("--background-color").trim();
+    }
+    return "#000000"; // Default color if not in the browser
+  }, []);
 
   const options: ISourceOptions = useMemo(
     () => ({
       background: {
         color: {
-          value: "#000000",
+          value: bgColor,
         },
       },
       fpsLimit: 120,
