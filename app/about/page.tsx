23c46b9cf0aa0de7fce6055a48f942ce0b161ca4
Jacob Maynard <jacobamaynard@proton.me>
2025-01-11 14:39:14 -0700

    improved loading in about me

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,17 +1,25 @@
+"use client";
 // major coursework
 // eagle scout
 // internship - bokeh image
 // math skills
 // other skills
 // skiing and hiking hobbies
-import AboutMe from "@/components/ui/about/about-me";
-import Projects from "@/components/ui/about/projects";
+import React, { Suspense, lazy } from "react";
+
+const AboutMe = lazy(() => import("@/components/ui/about/about-me"));
+const Projects = lazy(() => import("@/components/ui/about/projects"));
+import { LoadingSpinner } from "@/components/spinner";
 
 export default function Page() {
   return (
     <div className="flex flex-col items-center mx-4 mb-4 gap-2">
-      <AboutMe />
-      <Projects />
+      <Suspense fallback={<LoadingSpinner />}>
+        <AboutMe />
+      </Suspense>
+      <Suspense>
+        <Projects />
+      </Suspense>
     </div>
   );
 }
---
+++ components/spinner.tsx
@@ -0,0 +1,5 @@
+import { LoaderCircle } from "lucide-react";
+
+export const LoadingSpinner = () => {
+  return <LoaderCircle size={40} className="spin" />;
+};
