1e4ce0f0d7296e7326c1b731db2d677bc183f5e6
Jacob Maynard <jacobamaynard@proton.me>
2025-01-12 12:40:01 -0700

    formatting

--- app/hobbies/page.tsx
+++ app/hobbies/page.tsx
@@ -27,7 +27,7 @@ export default function Page() {
       <h1 className="text-3xl font-bold mb-6 text-center gradient-text animate-gradient bg-clip-text text-transparent">
         Graphic Design
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
         {designs.map((design) => (
           <motion.div
             key={design.id}
@@ -41,7 +41,7 @@ export default function Page() {
               transition={{ duration: 0.2, ease: "easeOut" }}
             >
               {isLoading && (
-                <div className="absolute inset-0 flex items-center justify-center bg-gray-200">
+                <div className="rounded-lg absolute inset-0 flex items-center justify-center bg-gray-200">
                   <LoaderCircle className="animate-spin" size={36} />
                 </div>
               )}
@@ -51,7 +51,7 @@ export default function Page() {
                   alt={design.alt}
                   width={400}
                   height={300}
-                  className={`object-cover object-center h-60 ${isLoading ? "opacity-0" : "opacity-100"}`}
+                  className={`rounded-lg object-cover object-center h-60 ${isLoading ? "opacity-0" : "opacity-100"}`}
                   style={{ transform: "scale(1.01)" }}
                   onLoad={() => setIsLoading(false)}
                 />
